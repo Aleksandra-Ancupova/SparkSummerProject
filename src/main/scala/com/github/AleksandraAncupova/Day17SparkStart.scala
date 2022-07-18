0c228c7305241a48a387f23f2bfd5bf0e1d91b1c
Aleksandra <alexandra.ancupova@gmal.com>
2022-07-18 19:38:02 +0300

    day 17 spark

--- src/main/scala/com/github/AleksandraAncupova/Day17SparkStart.scala
+++ src/main/scala/com/github/AleksandraAncupova/Day17SparkStart.scala
@@ -8,4 +8,13 @@ object Day17SparkStart extends App {
   val spark = SparkSession.builder().appName("test").master("local").getOrCreate()
   println(s"Session started in Spark version ${spark.version}")
 
+
+  val myRange = spark.range(1000).toDF("number")
+  val divisibleBy5 = myRange.where("number % 5 = 0")
+  divisibleBy5.show(10) // show first rows
+
+  val range100 = spark.range(100).toDF("numbers")
+  val divisibleBy10 = range100.where("numbers % 10 = 0")
+  divisibleBy10.show()
+
 }
