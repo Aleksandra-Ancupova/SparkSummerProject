95d5bf16103a75e7bdaf3c344904d73cc34f98e6
Aleksandra <alexandra.ancupova@gmal.com>
2022-08-02 14:58:43 +0300

    day 25 exercise

---
+++ src/main/scala/com/github/AleksandraAncupova/Day25Exercise.scala
@@ -0,0 +1,22 @@
+package com.github.AleksandraAncupova
+
+import com.github.AleksandraAncupova.SparkUtil.getSpark
+import org.apache.spark.sql.functions.{col, current_date, current_timestamp, datediff, months_between, round}
+
+object Day25Exercise extends App {
+  val spark = getSpark("Sparky")
+
+  val filePath = "src/resources/retail-data/by-day/2011-03-01.csv"
+
+  val df = SparkUtil.readCSVwithView(spark, filePath)
+
+  //TODO add new column with current date, timestamp, day diff and month diff
+
+  df.select("InvoiceDate")
+    .withColumn("today", current_date())
+    .withColumn("now", current_timestamp())
+    .withColumn("dayDiff", datediff(col("now"), col("InvoiceDate")))
+    .withColumn("monthlyDiff", round(months_between(col("now"), col("InvoiceDate"))))
+    .show(10, false)
+
+}
