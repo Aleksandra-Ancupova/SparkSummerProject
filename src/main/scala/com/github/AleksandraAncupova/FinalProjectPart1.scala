5dd5e839b8ad8b78deed55af1f120798f6fffe6b
Aleksandra <alexandra.ancupova@gmal.com>
2022-08-27 13:24:11 +0300

    all before model

--- src/main/scala/com/github/AleksandraAncupova/FinalProjectPart1.scala
+++ src/main/scala/com/github/AleksandraAncupova/FinalProjectPart1.scala
@@ -2,7 +2,7 @@ package com.github.AleksandraAncupova
 
 import com.github.AleksandraAncupova.SparkUtil.{getSpark, readDataWithView}
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{avg, col, desc, expr, max, min, round, stddev}
+import org.apache.spark.sql.functions.{avg, col, countDistinct, desc, expr, lit, max, min, round, sum, stddev}
 
 object FinalProjectPart1 extends App {
 
@@ -21,9 +21,11 @@ object FinalProjectPart1 extends App {
     .rowsBetween(Window.unboundedPreceding, Window.currentRow)
 
   val avgDailyReturn = avg(col("dailyReturn")).over(windowSpec)
+  val cumReturn = sum(col("dailyReturn")).over(windowSpec)
 
   val dfWithAvgReturn= dfWithReturn.select(col("*"),
-    round(avgDailyReturn,2).alias("avgDailyReturn"))
+    round(avgDailyReturn,2).alias("avgDailyReturn"),
+    round(cumReturn,2).alias("totalReturn"))
 
   dfWithAvgReturn.show()
 
@@ -40,15 +42,27 @@ object FinalProjectPart1 extends App {
       .option("header", true)
       .save("src/resources/final/my-csv-file.csv")
 
+  // this option shows average return of 5 stocks each day and total return of 5 stocks each day
+//      df.withColumn("daily_return", expr("round((close - open)/open * 100, 2)"))
+//      .groupBy("date")
+//      .agg(expr("round(avg(daily_return),2)").alias("Average Return per Stock"),
+//        expr("round(sum(daily_return), 2)").alias("Total Return of all Stocks")
+//      )
+//        .orderBy("date")
+//        .show()
+
   //Which stock was traded most frequently - as measured by closing price * volume - on average?
 
-  df.withColumn("frequency", expr("close * volume"))
+  val tradeFrequency = df.withColumn("frequency", expr("close * volume"))
     .groupBy("ticker")
     .agg(avg("frequency").alias("avgTradingFrequency"))
     .orderBy(desc("avgTradingFrequency"))
-    .show()
 
-  println("Apple Stock was the most frequently traded throughout given period")
+  tradeFrequency.show()
+
+  val mostTraded = tradeFrequency.collect().map(_.getString(0))
+  println(s"${mostTraded.head} stock was the most frequently traded throughout given period")
+
 
   //Which stock was the most volatile as measured by annualized standard deviation of daily returns?
 
@@ -57,15 +71,27 @@ object FinalProjectPart1 extends App {
 
   val SDForDailyReturn = stddev(col("dailyReturn")).over(windowSpec)
 
-  val dfVolatile = dfWithAvgReturn.select(col("*"),
+  val dfVolatile = dfWithReturn.select(col("*"),
     round(SDForDailyReturn,2).alias("stDevOfDailyReturn"))
 
-  dfVolatile.where("date = '2016-11-02'")
-    .withColumn("volatility", expr("round(stDevOfDailyReturn * sqrt(250), 2)"))
-    .orderBy(desc("volatility"))
-    .show()
+  //counting for many periods/date to calculate volatility
+  val dateCount = df.select(countDistinct("date")).collect().map(_.toSeq)
+  val dateCountInt = dateCount.head.head
+  println(dateCountInt)
 
-  println("Tesla Stock was the most volatile throughout given period: 35.26% volatility")
+  val testDF = dfVolatile.withColumn("dateCount", lit(dateCountInt))
+  testDF.show()
+
+  val dfYearlyVolatility = testDF.where("date = '2016-11-02'")
+    .withColumn("volatility", expr("round(stDevOfDailyReturn * sqrt(dateCount), 2)"))
+    .orderBy(desc("volatility"))
+
+  dfYearlyVolatility.show()
+
+  val volatilityArray = dfYearlyVolatility.collect().map(_.toSeq)
+  val highestVolatility = volatilityArray.head
+
+  println(s"${highestVolatility(6)} Stock was the most volatile throughout given period: ${highestVolatility.last}")
 
 
 }
