17bc321a570ce901ef40058bfedae23a8a1b6973
Aleksandra <alexandra.ancupova@gmal.com>
2022-08-27 16:14:53 +0300

    model without volume + scala like functions

---
+++ src/main/scala/com/github/AleksandraAncupova/FinalTest.scala
@@ -0,0 +1,73 @@
+package com.github.AleksandraAncupova
+
+import com.github.AleksandraAncupova.SparkUtil.{getSpark, readDataWithView}
+import org.apache.spark.ml.feature.RFormula
+import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{col, lead}
+
+object FinalTest extends App {
+  val spark = getSpark("Sparky")
+  val filePath = "src/resources/csv/stock_prices_.csv"
+
+  val df = readDataWithView(spark, filePath)
+
+  // creating an array of distinct stocks/tickers
+  val stocks = spark.sql(
+    """
+      |SELECT DISTINCT(ticker) FROM dfTable
+      |""".stripMargin)
+
+  val rows = stocks.collect()
+  val strings = rows.map(_.getString(0))
+  println(strings.mkString(","))
+
+  //main loop
+  for (st <- strings) {
+    //preprocessing data
+    val myDF = preprocessing(df, st)
+
+    //building a model, predicting
+    val rFormula = new RFormula()
+      .setFormula("nextDayClose ~ open + high + low + close")
+
+    val newDF = rFormula.fit(myDF).transform(myDF)
+
+    val Array(train, test) = newDF.randomSplit(Array(0.8, 0.2))
+
+    val linReg = new LinearRegression()
+    val lrModel = linReg.fit(train)
+
+    val predictDF = lrModel.transform(test)
+    predictDF.show()
+
+    //evaluating the model
+    testLinearRegression(lrModel)
+
+  }
+
+
+  def preprocessing(df: DataFrame, stockName: String): DataFrame = {
+    val stock = df.where(col("ticker") === stockName)
+
+    val windowSpec = Window.partitionBy("ticker").orderBy("date")
+    val nextDayClose = lead(col("close"), 1).over(windowSpec)
+
+    val stockDF = stock.withColumn("nextDayClose", nextDayClose)
+
+    val stockDFNoNull = stockDF.na.drop
+    stockDFNoNull
+  }
+
+
+  def testLinearRegression(model: LinearRegressionModel): Unit = {
+    val intercept = model.intercept
+    val coefficient = model.coefficients
+    val mae = model.summary.meanAbsoluteError
+    val mse = model.summary.meanSquaredError
+    println(s"The model has following intercept: $intercept; coefficients: $coefficient; MAE: $mae; MSE: $mse")
+
+  }
+
+}
